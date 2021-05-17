d4a423ea0ab3abfed859d6adf88fb7cbf1bdcbda
rubencobollo <76393299+rubencobollo@users.noreply.github.com>
2021-05-17 09:12:32 +0200

    Update leerFicheroXML.js

--- ejercicio1/leerFicheroXML.js
+++ ejercicio1/leerFicheroXML.js
@@ -4,7 +4,8 @@ function gestionarFicheroXML(xmlDoc){
 	for(let i=0; i<libros.length; i++)
 		if(i == 2)
 		capaVacia.innerHTML = capaVacia.innerHTML + "<p class='seleccionado'>" + libros[i].textContent + "</p>"
-	
+		else
+		capaVacia.innerHTML = capaVacia.innerHTML + "<p>" + libros[i].textContent + "</p>"	
 }
 
 loadDocA("libros.xml","xml");
