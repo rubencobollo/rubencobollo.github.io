55d144ea85960fef2b8469d91a218433e1b802e9
rubencobollo <76393299+rubencobollo@users.noreply.github.com>
2021-05-21 09:34:27 +0200

    Update leerFicheroXML.js

--- Examen/leerFicheroXML.js
+++ Examen/leerFicheroXML.js
@@ -1,18 +1,21 @@
 function gestionarFicheroXML(xmlDoc){
 	let capaVacia = document.querySelector("#ficheroXML")
 	capaVacia.innerHTML =""
-	let emisor = xmlDoc.querySelectorAll("mensaje1")
-	let receptor = xmlDoc.querySelectorAll("mensaje2")
-	for(let i=0; i<emisor.length; i++)
+	let cartelera = xmlDoc.querySelectorAll("pelicula")
+	let titulos = xmlDoc.querySelectorAll("titulo")
+	let directores = xmlDoc.querySelectorAll("director")
+	let estrenos = xmlDoc.querySelectorAll("estreno")
+	for(let i=0; i<cartelera.length; i++)
 	{
-		capaVacia.innerHTML = capaVacia.innerHTML + "<p class='mensaje1'>" + emisor[i].textContent + "</p>"
-		capaVacia.innerHTML = capaVacia.innerHTML + "<p class='mensaje2'>" + receptor[i].textContent + "</p>"
-
+		capaVacia.innerHTML = capaVacia.innerHTML + "<p class='detalle'>" + cartelera[i].textContent + "</p>"
+		capaVacia.innerHTML = capaVacia.innerHTML + "<p>" + titulos[i].textContent + "</p>"
+		capaVacia.innerHTML = capaVacia.innerHTML + "<p>" + directores[i].textContent + "</p>"
+		capaVacia.innerHTML = capaVacia.innerHTML + "<p>" + estrenos[i].textContent + "</p>"
 	}
 }
 
 function CargarFichero()
 {
-	loadDocA("whatsapp.xml","xml")
+	loadDocA("cine.xml","xml")
 }
 setInterval(CargarFichero,5000)
